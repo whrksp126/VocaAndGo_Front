a91cac9cff9a53752edfe56b792ae4cac3e5b542
whrksp126 <whrksp126@gmail.com>
2024-09-10 02:40:09 +0900

    save

--- js/osFunction.js
+++ js/osFunction.js
@@ -21,6 +21,6 @@ function getDevicePlatform() {
 }
 
 window.addEventListener('message', function(event) {
+  // Directly using the message, no need for JSON.parse
   alert('Received message: ' + event.data);
-
 });
