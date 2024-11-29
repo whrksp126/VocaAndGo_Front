76b66bac554cb1b48bf0dd48dece10cb62805def
whrksp126 <whrksp126@gmail.com>
2024-11-30 00:51:37 +0900

    save

--- js/osFunction.js
+++ js/osFunction.js
@@ -122,7 +122,6 @@ function setSqliteQuery(query, params = []) {
   };
   return new Promise((resolve, reject) => {
     // WebView로 메시지 전송
-    alert('3')
     window.ReactNativeWebView?.postMessage(JSON.stringify(queryData));
     const handleMessage = function (event) {
       try {
