f8c8c393b2822a6561113e6e9106308a5297c235
whrksp126 <whrksp126@gmail.com>
2024-11-30 19:45:12 +0900

    save

--- js/indexedDb.js
+++ js/indexedDb.js
@@ -356,8 +356,8 @@ async function getWordbook(id = null) {
   if(getDevicePlatform() == "app"){
     const result = await setSqliteQuery(selectQuery, params)
     alert(JSON.stringify(result))
-    if (result?.length > 0) {
-      const data = result.map(({ id, name, color, status, createdAt, updatedAt }) => ({
+    if (result?.rows?.length > 0) {
+      const data = result.rows.map(({ id, name, color, status, createdAt, updatedAt }) => ({
         id,
         name,
         color: color ? JSON.parse(color) : {},
