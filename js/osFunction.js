589a81f43164356b14eba58fbe6dc61830b700a3
whrksp126 <whrksp126@gmail.com>
2024-11-30 17:42:29 +0900

    save

--- js/osFunction.js
+++ js/osFunction.js
@@ -126,7 +126,7 @@ function setSqliteQuery(query, params = []) {
     const handleMessage = function (event) {
       try {
         const message = JSON.parse(event.data);
-        alert(event.data)
+        alert(`hadleMessage : ${event.data}`)
         if (message.type === 'sqlite_query_return') {
           if(message.success){
             resolve(message.result); 
