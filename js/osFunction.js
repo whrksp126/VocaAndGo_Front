190e0a67e2a54ce7958b024eda80ac513eee859c
whrksp126 <whrksp126@gmail.com>
2024-12-13 01:26:36 +0900

    save

--- js/osFunction.js
+++ js/osFunction.js
@@ -173,6 +173,7 @@ function getSqliteStatus() {
 // 네이티브 cofirm 세팅
 function setConfirm(data) {
   if(getDevicePlatform() == "app"){
+    alert("app");
     return new Promise((resolve, reject) => {
       // WebView로 메시지 전송
       window.ReactNativeWebView?.postMessage(JSON.stringify({
